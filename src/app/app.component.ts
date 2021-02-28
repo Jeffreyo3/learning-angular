91da1f2a5419a3ec34ba82886421588dfb2b712f
jeffreyo3 <jeffrey.orndorff@gmail.com>
2021-02-28 12:23:09 -0800

    Learn template basics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // template:'<h1>Artist Directory</h1>', // cannot multi-line for 'template'. must use back-ticks
+  // template:`
+  // <h1>Artist Directory</h1>
+  // <div>Hello World</div>
+  // `,
+  styleUrls: []
 })
 export class AppComponent {
   title = 'Learning Angular';
